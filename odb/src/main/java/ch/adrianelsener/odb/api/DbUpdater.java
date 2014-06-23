f872981efabf0a52e0ffe8bd5686f8b91804bb2f
adrian.elsener@gmail.com <Herk2ules>
2014-06-23 06:52:50 +0200

    Added bus to collect and tranfer data

--- odb/src/main/java/ch/adrianelsener/odb/api/DbUpdater.java
+++ odb/src/main/java/ch/adrianelsener/odb/api/DbUpdater.java
@@ -1,11 +1,9 @@
 package ch.adrianelsener.odb.api;
 
-import ch.adrianelsener.odb.api.Odb;
-import ch.adrianelsener.train.gui.swing.TrackPart;
 
 /**
  * Created by els on 5/20/14.
  */
-public interface DbUpdater {
-    void apply(Odb<TrackPart> db);
+public interface DbUpdater<T extends Datacontainer> {
+    void apply(Odb<T> db);
 }
