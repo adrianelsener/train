e1ee974935a2a296f257efcb1957d65be67d36a3
adrian.elsener@gmail.com <Herk2ules>
2014-06-30 06:52:40 +0200

    added MenuItemBuilder to simplify creating menu items

--- ui/src/main/java/ch/adrianelsener/train/gui/swing/common/MenuItemBuilder.java
+++ ui/src/main/java/ch/adrianelsener/train/gui/swing/common/MenuItemBuilder.java
@@ -1,7 +1,31 @@
 package ch.adrianelsener.train.gui.swing.common;
 
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
 /**
- * Created by els on 6/25/14.
+ * Simple builder to create MenuItems.
  */
 public class MenuItemBuilder {
+    private final String text;
+    private ActionListener actionListener;
+
+    private MenuItemBuilder(String text) {
+        this.text = text;
+    }
+
+    public static MenuItemBuilder create(final String text) {
+        return new MenuItemBuilder(text);
+    }
+
+    public MenuItemBuilder addActionListener(ActionListener listener) {
+        actionListener = listener;
+        return this;
+    }
+
+    public JMenuItem build() {
+        final JMenuItem item = new JMenuItem(text);
+        item.addActionListener(actionListener);
+        return item;
+    }
 }
