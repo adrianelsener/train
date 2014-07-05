bc4b5273780681a350307754348307b8694d4ed3
els <els@eeelin.(none)>
2014-07-05 21:49:24 +0200

    removed path to mvn

--- SNMPAPI/jars/installToMvn.sh
+++ SNMPAPI/jars/installToMvn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-~/tools/mvn/mvn-latest/bin/mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=logging -Dversion=SNAPSHOT-1.0.0 -Dfile=AdventNetLogging.jar -Dpackaging=jar -DgeneratePom=true
-~/tools/mvn/mvn-latest/bin/mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=snmp -Dversion=SNAPSHOT-1.0.0 -Dfile=AdventNetSnmp.jar -Dpackaging=jar -DgeneratePom=true
-~/tools/mvn/mvn-latest/bin/mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=crimson -Dversion=SNAPSHOT-1.0.0 -Dfile=crimson.jar -Dpackaging=jar -DgeneratePom=true
-~/tools/mvn/mvn-latest/bin/mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=jaxp -Dversion=SNAPSHOT-1.0.0 -Dfile=jaxp.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=logging -Dversion=SNAPSHOT-1.0.0 -Dfile=AdventNetLogging.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=snmp -Dversion=SNAPSHOT-1.0.0 -Dfile=AdventNetSnmp.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=crimson -Dversion=SNAPSHOT-1.0.0 -Dfile=crimson.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=ch.adrianelsener.adventnet -DartifactId=jaxp -Dversion=SNAPSHOT-1.0.0 -Dfile=jaxp.jar -Dpackaging=jar -DgeneratePom=true
 
