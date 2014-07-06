2f883a3eeda6fa53d191668a26406493e41a60f2
adrian.elsener@gmail.com <Herk2ules>
2014-07-06 08:58:45 +0200

    removed unused and unknown import

--- ui/src/main/java/ch/adrianelsener/train/gui/swing/events/UpdateMainUi.java
+++ ui/src/main/java/ch/adrianelsener/train/gui/swing/events/UpdateMainUi.java
@@ -1,10 +1,5 @@
 package ch.adrianelsener.train.gui.swing.events;
 
-import com.sun.deploy.net.UpdateTracker;
-
-/**
- * Created by els on 5/20/14.
- */
 public class UpdateMainUi {
     private UpdateMainUi() {
         super();
