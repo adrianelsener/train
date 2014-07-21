fdb455a94be7ae5341d355bedc13c59517810cd6
adrian.elsener@gmail.com <Herk2ules>
2014-07-21 14:59:43 +0200

    first draft of MockInitializer Rule

--- testngrules/src/main/java/ch/adrianelsener/testing/MockInitializer.java
+++ testngrules/src/main/java/ch/adrianelsener/testing/MockInitializer.java
@@ -1,7 +1,25 @@
 package ch.adrianelsener.testing;
 
-/**
- * Created by els on 7/17/14.
- */
-public class MockInitializer {
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.mockito.MockitoAnnotations;
+
+public abstract class MockInitializer extends TestWatcher {
+    public static MockInitializer createMockitoInitializer(Object toInitialize) {
+        return new MockitoInitializer(toInitialize);
+    }
+
+    private static class MockitoInitializer extends MockInitializer {
+
+        private final Object toInitialize;
+
+        MockitoInitializer(Object toInitialize) {
+            this.toInitialize = toInitialize;
+        }
+
+        @Override
+        protected void starting(Description description) {
+            MockitoAnnotations.initMocks(toInitialize);
+        }
+    }
 }
