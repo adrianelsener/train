c6d3aadbde6b0636362537d9ef1e3d68ad9fad61
els <adrian.elsener@gmail.com>
2014-11-08 16:19:37 +0100

    Simple information in popup about switchId

--- ui/src/main/java/ch/adrianelsener/train/gui/swing/MainPopupMenu.java
+++ ui/src/main/java/ch/adrianelsener/train/gui/swing/MainPopupMenu.java
@@ -1,44 +1,71 @@
 package ch.adrianelsener.train.gui.swing;
 
+import ch.adrianelsener.odb.api.Odb;
 import ch.adrianelsener.train.gui.swing.common.MenuItemBuilder;
+import ch.adrianelsener.train.gui.swing.model.TrackPart;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Optional;
 
 public class MainPopupMenu extends JPopupMenu {
+    private static final Logger logger = LoggerFactory.getLogger(MainPopupMenu.class);
     private EventBus bus;
+    private Optional<TrackPart> optPart = Optional.empty();
+    private final JTextField txtBoardId = new JTextField();
 
     public MainPopupMenu() {
         for (DrawMode drawMode : DrawMode.values()) {
             add(MenuItemBuilder.create(drawMode.name()).addActionListener(e -> bus.post(drawMode)).build());
         }
+        add(txtBoardId);
+        txtBoardId.setVisible(false);
     }
 
     public void setBus(EventBus bus) {
         this.bus = bus;
     }
 
+    public void setPart(Optional<TrackPart> newPart) {
+        logger.debug("set popup menu part to {}", newPart);
+        this.optPart = newPart;
+        logger.debug("evaluted part '{}' for popupmenu", optPart);
+        optPart.ifPresent(part -> {
+            txtBoardId.setText(part.getBoardId().toString());
+            txtBoardId.setVisible(true);
+        });
+    }
+
 
     public static class PopupClickListener extends MouseAdapter {
         @Inject
         private EventBus bus;
+        @Inject
+        private Odb<TrackPart> db;
+
         final MainPopupMenu menu = new MainPopupMenu();
 
         public void mousePressed(MouseEvent e) {
-            if (e.isPopupTrigger())
+            if (e.isPopupTrigger()) {
                 doPop(e);
+            }
         }
 
         public void mouseReleased(MouseEvent e) {
-            if (e.isPopupTrigger())
+            if (e.isPopupTrigger()) {
                 doPop(e);
+            }
         }
 
         private void doPop(MouseEvent e) {
             menu.setBus(bus);
+            Optional<TrackPart> partNearBy = db.filterUnique(part -> part.isNear(e.getPoint()));
+            menu.setPart(partNearBy);
             menu.show(e.getComponent(), e.getX(), e.getY());
         }
     }
