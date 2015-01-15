b965ddc0c342a2865bbf3ce53d283ac9dc99fe70
adrian.elsener@gmail.com <herk2ules>
2015-01-15 06:22:08 +0100

    finished impl of switch board v2

--- driver/src/main/java/ch/adrianelsener/train/driver/SwitchBoardV2.java
+++ driver/src/main/java/ch/adrianelsener/train/driver/SwitchBoardV2.java
@@ -2,9 +2,16 @@ package ch.adrianelsener.train.driver;
 
 import ch.adrianelsener.train.config.Config;
 import ch.adrianelsener.train.denkovi.Board;
+import ch.adrianelsener.train.denkovi.Pin;
+import ch.adrianelsener.train.denkovi.PinState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class SwitchBoardV2 extends AbstractBoard implements SwitchBoardDriver {
+    private static final Logger logger = LoggerFactory.getLogger(SwitchBoardV2.class);
     private Config boardProps;
+
 
     public SwitchBoardV2(final Board board, final Config config, final int boardNumber) {
         super(board, config, boardNumber);
@@ -17,7 +24,10 @@ public class SwitchBoardV2 extends AbstractBoard implements SwitchBoardDriver {
 
     @Override
     public void turn(SwitchWithState statedRelay) {
-
+        setStatePins(statedRelay);
+        final String toggleNumber = getBoardCfg().getChild("T");
+        final Pin togglePin = Pin.forPDotNotation(toggleNumber);
+        togglePin(togglePin);
     }
 
     @Override
@@ -25,4 +35,32 @@ public class SwitchBoardV2 extends AbstractBoard implements SwitchBoardDriver {
         return true;
     }
 
+    private void setStatePins(final SwitchWithState statedRelay) {
+        setBoardSelectionPins(statedRelay);
+        setStateSelectionPins(statedRelay);
+    }
+
+    private void setStateSelectionPins(SwitchWithState statedRelay) {
+        for (int i = 0; i < 4; i++) {
+            setPin(statedRelay, "S", i);
+        }
+    }
+
+    private void setBoardSelectionPins(SwitchWithState statedRelay) {
+        for (int i = 0; i < 4; i++) {
+            setPin(statedRelay, "B", i);
+        }
+    }
+
+    private void setPin(final SwitchWithState statedRelay, final String prefix, final int i) {
+        final PinState pinState = getPinStateForId(statedRelay, prefix, i);
+        logger.debug("Set {}", pinState);
+        set(pinState);
+    }
+
+    private PinState getPinStateForId(final SwitchWithState statedRelay, final String prefix, final int i) {
+        final String mappedPin = getBoardCfg().getChild(prefix + (i + 1)).substring(2);
+        final Pin pin1 = Pin.forNumber(mappedPin);
+        return statedRelay.getStates().get(i).forPin(pin1);
+    }
 }
--- driver/src/test/java/ch/adrianelsener/train/driver/SwitchBoardV2Test.java
+++ driver/src/test/java/ch/adrianelsener/train/driver/SwitchBoardV2Test.java
@@ -3,11 +3,26 @@ package ch.adrianelsener.train.driver;
 import ch.adrianelsener.train.config.ConfKey;
 import ch.adrianelsener.train.config.Config;
 import ch.adrianelsener.train.denkovi.Board;
+import ch.adrianelsener.train.denkovi.Pin;
+import ch.adrianelsener.train.denkovi.PinState;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InOrder;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -37,19 +52,99 @@ public class SwitchBoardV2Test {
         assertThat(result, equalTo(expectedConfig));
     }
 
+    @Test(dataProvider = "leftAndRight")
+    public void turn(final Testdata testData) {
+        final Board board = mock(Board.class);
+        final Config config = testData.config;
+
+        final SwitchBoardV2 testee = new SwitchBoardV2(board, config, 0);
+        // Act
+        testee.turn(testData.weicheSeite);
+        // Assert
+        testData.verify(board);
+    }
+
+    @DataProvider(name = "leftAndRight", parallel = true)
+    public Object[][] createLeftRightData() {
+        final Config config = createSampleConfig();
+        config.put(ConfKey.create("RB.00.B"), "01");
+        PinState p09 = PinState.Pon(Pin._09);
+        PinState p10 = PinState.Pon(Pin._10);
+        PinState p11 = PinState.Pon(Pin._11);
+        PinState p12 = PinState.Pon(Pin._12);
+        PinState p13 = PinState.Pon(Pin._13);
+        PinState p14 = PinState.Pon(Pin._14);
+        PinState p15 = PinState.Pon(Pin._15);
+        PinState p16 = PinState.Pon(Pin._16);
+        Pin p17 = Pin._17;
+        return new Object[][]{//
+                {new Testdata(SwitchWithState._01L, p09, p10, p11, p12,p13, p14, p15, p16, p17, config)}//
+        };
+    }
+
 
     private Config createSampleConfig() {
         final Config config = new Config();
-        config.put(ConfKey.create("RB.00.S1"), "P.10");
-        config.put(ConfKey.create("RB.00.S2"), "P.11");
-        config.put(ConfKey.create("RB.00.S3"), "P.12");
-        config.put(ConfKey.create("RB.00.S4"), "P.13");
-        config.put(ConfKey.create("RB.00.B1"), "P.14");
-        config.put(ConfKey.create("RB.00.B2"), "P.15");
-        config.put(ConfKey.create("RB.00.B3"), "P.16");
-        config.put(ConfKey.create("RB.00.B4"), "P.17");
-        config.put(ConfKey.create("RB.00.T"), "P.18");
+        config.put(ConfKey.create("RB.00.S1"), "P.09");
+        config.put(ConfKey.create("RB.00.S2"), "P.10");
+        config.put(ConfKey.create("RB.00.S3"), "P.11");
+        config.put(ConfKey.create("RB.00.S4"), "P.12");
+        config.put(ConfKey.create("RB.00.B1"), "P.13");
+        config.put(ConfKey.create("RB.00.B2"), "P.14");
+        config.put(ConfKey.create("RB.00.B3"), "P.15");
+        config.put(ConfKey.create("RB.00.B4"), "P.16");
+        config.put(ConfKey.create("RB.00.T"), "P.17");
         return config;
     }
+
+    private static class Testdata {
+
+        private final SwitchWithState weicheSeite;
+        private final PinState s1;
+        private final PinState s2;
+        private final PinState s3;
+        private final PinState s4;
+        private final PinState b1;
+        private final PinState b2;
+        private final PinState b3;
+        private final PinState b4;
+        private final Pin ptoggle;
+        private final Config config;
+
+        public Testdata(final SwitchWithState weicheSeite, final PinState s1, final PinState s2, final PinState s3, final PinState s4,final PinState b1, final PinState b2, final PinState b3, final PinState b4,
+                        final Pin ptoggle, final Config config) {
+            this.weicheSeite = weicheSeite;
+            this.s1 = s1;
+            this.s2 = s2;
+            this.s3 = s3;
+            this.s4 = s4;
+            this.b1 = b1;
+            this.b2 = b2;
+            this.b3 = b3;
+            this.b4 = b4;
+            this.ptoggle = ptoggle;
+            this.config = config;
+        }
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+        }
+
+        public void verify(final Board board) {
+            final InOrder order = inOrder(board);
+            order.verify(board).set(eq(b1));
+            order.verify(board).set(eq(b2));
+            order.verify(board).set(eq(b3));
+            order.verify(board).set(eq(b4));
+            order.verify(board).set(eq(s1));
+            order.verify(board).set(eq(s2));
+            order.verify(board).set(eq(s3));
+            order.verify(board).set(eq(s4));
+            order.verify(board).set(PinState.Pon(ptoggle));
+            order.verify(board).set(PinState.Poff(ptoggle));
+        }
+    }
+
 
 }
