f4281a851a9776ab958ba16b20bfc04ba5ac6385
adrian.elsener@gmail.com <adrian.elsener@gmail.com>
2015-04-08 21:37:39 +0200

    read from console

--- pi/src/main/java/ch/adrianelsener/train/pi/PwmTest.java
+++ pi/src/main/java/ch/adrianelsener/train/pi/PwmTest.java
@@ -1,7 +1,57 @@
 package ch.adrianelsener.train.pi;
 
+import com.pi4j.wiringpi.Gpio;
+import com.pi4j.wiringpi.SoftPwm;
+
+import java.io.Console;
+import java.util.Scanner;
+
 /**
  * Created by els on 08.04.15.
  */
 public class PwmTest {
+    private static int PIN_NUMBER = 1;
+
+    public static void main(String[] args) throws InterruptedException {
+        // initialize wiringPi library, this is needed for PWM
+        Gpio.wiringPiSetup();
+
+        // softPwmCreate(int pin, int value, int range)
+        // the range is set like (min=0 ; max=100)
+        SoftPwm.softPwmCreate(PIN_NUMBER, 0, 100);
+        int value = 50;
+        String line = "";
+        Scanner scan = new Scanner(System.in);
+        while(!line.contains("e")) {
+            SoftPwm.softPwmWrite(PIN_NUMBER, value);
+            line = scan.nextLine();
+            if (line.contains("+")) {
+                value += 5;
+            } else if (line.contains("-")) {
+                value -= 5;
+            }
+        }
+//        int counter = 0;
+//        while (counter < 3000) {
+//
+//            // fade LED to fully ON
+//            for (int i = 0; i <= 100; i++) {
+//                // softPwmWrite(int pin, int value)
+//                // This updates the PWM value on the given pin. The value is
+//                // checked to be in-range and pins
+//                // that haven't previously been initialized via softPwmCreate
+//                // will be silently ignored.
+//                SoftPwm.softPwmWrite(PIN_NUMBER, i);
+//                Thread.sleep(25);
+//            }
+//
+//            // fade LED to fully OFF
+//            for (int i = 100; i >= 0; i--) {
+//                SoftPwm.softPwmWrite(PIN_NUMBER, i);
+//                Thread.sleep(25);
+//            }
+//
+//            counter++;
+//        }
+    }
 }
