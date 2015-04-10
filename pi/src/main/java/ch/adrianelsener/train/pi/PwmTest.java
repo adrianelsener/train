85bd16d9a71f93d0888e68fb813fbc34e2a8ae7d
adrian.elsener@gmail.com <adrian.elsener@gmail.com>
2015-04-10 20:11:38 +0200

    pin 4 instead of 0

--- pi/src/main/java/ch/adrianelsener/train/pi/PwmTest.java
+++ pi/src/main/java/ch/adrianelsener/train/pi/PwmTest.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * Created by els on 08.04.15.
  */
 public class PwmTest {
-    private static int PIN_NUMBER = 0;
+    private static int PIN_NUMBER = 4;
 
     public static void main(String[] args) throws Exception {
         // initialize wiringPi library, this is needed for PWM
