738ce94a44a38b9889f2c558ec8e13bd901c43bd
adrian.elsener@gmail.com <herk2ules>
2015-04-24 14:19:49 +0200

    do something for code readability

--- avr/src/main/Serial.c
+++ avr/src/main/Serial.c
@@ -18,7 +18,6 @@
 #define LOW_BYTE(x)        	(x & 0xff)					// 16Bit 	--> 8Bit
 #define HIGH_BYTE(x)       	((x >> 8) & 0xff)			// 16Bit 	--> 8Bit
 
-
 #define sbi(ADDRESS,BIT) 	((ADDRESS) |= (1<<(BIT)))	// set Bit
 #define cbi(ADDRESS,BIT) 	((ADDRESS) &= ~(1<<(BIT)))	// clear Bit
 #define	toggle(ADDRESS,BIT)	((ADDRESS) ^= (1<<BIT))		// Bit umschalten
@@ -28,59 +27,54 @@
 
 
 //###################### Variablen
-	uint16_t 	Variable=2345;				//Z�hler
+	uint16_t 	Variable=2345;				//Counter
 	uint16_t	buffer;
 	uint16_t	low, hight;
 
-//################################################################################################### Initialisierung
-void Initialisierung(void)
-	{
+void Initialisierung(void) {
 	cli();
 	//### PORTS
 
 	//### TWI
-	init_twi_slave(SLAVE_ADRESSE);			//TWI als Slave mit Adresse slaveadr starten
+	init_twi_slave(SLAVE_ADRESSE);			//TWI als Slave mit Adresse SLAVE_ADRESSE starten
 
 	sei();
+}
+
+int main(void) {
+
+	Initialisierung();
+
+	while(1) {
+		//############################ write Data in txbuffer
+
+		// 8Bit variable
+		txbuffer[2]=3;
+		txbuffer[3]=4;
+		txbuffer[4]=5;
+		txbuffer[5]=6;
+
+		// 16Bit Variable --> 2x 8Bit Variable
+		buffer		= Variable;
+		txbuffer[0]	= LOW_BYTE(buffer);			//16bit --> 8bit
+		txbuffer[1]	= HIGH_BYTE(buffer);		//16bit --> 8bit
+
+
+		//############################ read Data form rxbuffer
+
+		// 8Bit variable
+		Variable	= rxbuffer[2];
+
+		// 2x 8Bit Variable -->16Bit Variable
+		low			= rxbuffer[0];
+		hight		= rxbuffer[1];
+		Variable	= uniq(low,hight);			// 2x 8Bit  --> 16Bit
+
+		_delay_ms(500);
+
+		//############################
 	}
-
-//################################################################################################### Hauptprogramm
-int main(void)
-{
-
-Initialisierung();
-
-while(1)
-    {
-	//############################ write Data in txbuffer
-
-	// 8Bit variable
-	txbuffer[2]=3;
-	txbuffer[3]=4;
-	txbuffer[4]=5;
-	txbuffer[5]=6;
-
-	// 16Bit Variable --> 2x 8Bit Variable
-	buffer		= Variable;
-	txbuffer[0]	= LOW_BYTE(buffer);			//16bit --> 8bit
-	txbuffer[1]	= HIGH_BYTE(buffer);		//16bit --> 8bit
-
-
-	//############################ read Data form rxbuffer
-
-	// 8Bit variable
-	Variable	= rxbuffer[2];
-
-	// 2x 8Bit Variable -->16Bit Variable
-	low			= rxbuffer[0];
-	hight		= rxbuffer[1];
-	Variable	= uniq(low,hight);			// 2x 8Bit  --> 16Bit
-
-	_delay_ms(500);
-
-	//############################
-	} //end.while
-} //end.main
+}
 
 
 
