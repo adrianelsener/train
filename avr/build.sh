eb0e31a165ae102a6a0b522fa77a80c266894c4e
adrian.elsener@gmail.com <herk2ules>
2015-04-24 14:06:34 +0200

    fix some issues in build for tiny

--- avr/build.sh
+++ avr/build.sh
@@ -6,3 +6,8 @@ fi
 avr-g++ src/main/led_fader_t13_code/test1.c -mmcu=attiny2313 -Os -o target/main/led_fader_t13_code/fader.elf
 avr-objcopy -O ihex target/main/led_fader_t13_code/fader.elf target/main/led_fader_t13_code/fader.hex
 echo 'sudo avrdude -p t2313 -e -c avr910 -P /dev/ttyUSB0 -U flash:w:target/main/led_fader_t13_code/fader.hex:i'
+
+
+avr-g++ src/main/Serial.c -mmcu=attiny2313 -Os -o target/main/Serial.elf
+avr-objcopy -O ihex target/main/Serial.elf target/main/Serial.hex
+echo 'sudo avrdude -p t2313 -e -c avr910 -P /dev/ttyUSB0 -U flash:w:target/main/Serial.hex:i'
