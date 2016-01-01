0d44949633fbb39afa483f5a0fcd06eec43859c2
adrian.elsener@gmail.com <adrian.elsener@gmail.com>
2016-01-01 15:04:25 +0100

    fix byte size

--- pi/src/test/java/ch/adrianelsener/train/pi/twi/TwiTest.java
+++ pi/src/test/java/ch/adrianelsener/train/pi/twi/TwiTest.java
@@ -62,7 +62,7 @@ public class TwiTest {
     }
 
     private void read(Optional<I2CDevice> i2CDevice) throws IOException {
-        byte[] bytes = new byte[3];
+        byte[] bytes = new byte[4];
         i2CDevice.get().read(bytes, 0, bytes.length);
         System.out.printf("current pwm : %s\n", Byte.toUnsignedInt(bytes[0]));
         System.out.printf("dest pwm %s\n", Byte.toUnsignedInt(bytes[1]));
