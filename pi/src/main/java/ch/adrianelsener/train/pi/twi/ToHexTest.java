4a259191bf5f6aa91685196a7d9cbaee424a66b9
adrian.elsener@gmail.com <adrian.elsener@gmail.com>
2016-01-08 23:11:14 +0100

    Test

---
+++ pi/src/main/java/ch/adrianelsener/train/pi/twi/ToHexTest.java
@@ -0,0 +1,10 @@
+package ch.adrianelsener.train.pi.twi;
+
+/**
+ * Created by els on 08.01.16.
+ */
+public class ToHexTest {
+    public static final void main(String[] args) {
+        System.out.println(Integer.toHexString(250));
+    }
+}
