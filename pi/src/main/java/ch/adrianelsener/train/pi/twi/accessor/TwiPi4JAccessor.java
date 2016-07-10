03e2f0bf0b8e8d598cfe60a0177498444c3c2657
adrian.elsener@gmail.com <adrian.elsener@gmail.com>
2016-07-10 15:16:23 +0200

    add exception

--- pi/src/main/java/ch/adrianelsener/train/pi/twi/accessor/TwiPi4JAccessor.java
+++ pi/src/main/java/ch/adrianelsener/train/pi/twi/accessor/TwiPi4JAccessor.java
@@ -20,10 +20,10 @@ public class TwiPi4JAccessor implements TwiAccessor {
         super();
     }
 
-    private I2CBus getI2CBus() {
+    private I2CBus getI2CBus()  {
         try {
             return I2CFactory.getInstance(I2CBus.BUS_1);
-        } catch (IOException e) {
+        } catch (IOException | I2CFactory.UnsupportedBusNumberException e) {
             throw new TwiInitializationException(e);
         }
     }
