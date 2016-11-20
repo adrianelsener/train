327823745e7c35258ed06308f649d79cf7d41f04
Adrian Elsener <adrian.elsener@gmail.com>
2016-11-20 16:56:11 +0100

    Additional Log

--- pi/src/main/java/ch/adrianelsener/train/pi/twi/accessor/cmd/CmdSetter.java
+++ pi/src/main/java/ch/adrianelsener/train/pi/twi/accessor/cmd/CmdSetter.java
@@ -2,10 +2,15 @@ package ch.adrianelsener.train.pi.twi.accessor.cmd;
 
 import ch.adrianelsener.train.pi.dto.properties.TwiDevice;
 import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
 abstract class CmdSetter {
+    public static final Logger logger = LoggerFactory.getLogger(CmdSetter.class);
     private final static int SET_OFFSET = 4;
     @Inject
     private CmdExecutor cmdExecutor = new CmdExecutor();
@@ -16,6 +21,7 @@ abstract class CmdSetter {
         this.subDev = device.getSubDevice();
         this.devNr = device.getDeviceBusNr();
         final ImmutableList<String> parameters = createCommandList();
+        logger.debug("Before executing: {}", this);
         return cmdExecutor.executeSetCmd(parameters);
     }
 
@@ -33,4 +39,9 @@ abstract class CmdSetter {
     protected abstract int dataPosition();
 
     protected abstract int dataToSet();
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
 }
